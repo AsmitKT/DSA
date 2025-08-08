002148752ee14ca4470a7a8fd4cc2b7f8828160f
Asmit <asmitkr108@gmail.com>
2025-08-08 12:22:37 +0530

    added all comparison sorting algos

--- C++/BubbleSort.cpp
+++ C++/BubbleSort.cpp
@@ -1,25 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void selectionSort(int arr[], int n)
+void bubbleSort(int arr[], int n)
 {
     for (int i = 0; i < n-1; i++)
-    {
-        int min_idx = i;
-        for (int j = i+1; j < n; j++)
-        {
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
-        }
-        swap(arr[i], arr[min_idx]);
-    }
+        for (int j = 0; j < n-i-1; j++)
+            if (arr[j] > arr[j+1])
+                swap(arr[j], arr[j+1]);
 }
 
 int main()
 {
     int arr[] = {12, 11, 13, 5, 6};
     int n = sizeof(arr)/sizeof(arr[0]);
-    selectionSort(arr, n);
+    bubbleSort(arr, n);
     cout << "Sorted array: ";
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
